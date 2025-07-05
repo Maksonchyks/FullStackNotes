33e37232d72ff7320fea50ba825c02ac46a9540b
Maksym <maksonchyks@gmail.com>
2025-07-05 22:27:54 +0300

    extend note entity for change properties

---
+++ backend/MyFullStackNotes.Domain/Entities/Note.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFullStackNotes.Domain.Entities
+{
+    public class Note
+    {
+        public Guid Id { get; init; } = Guid.NewGuid();
+        public string Title { get; private set; }
+        public string? Description { get; private set; }
+        public DateTime CreatedAt { get; private set; } = DateTime.UtcNow;
+        public Guid UserId { get; private set; }
+        public User? User { get; private set; }
+        public Note(string title, string? description, Guid userId)
+        {
+            Title = title;
+            Description = description;
+            UserId = userId;
+        }
+    }
+}
