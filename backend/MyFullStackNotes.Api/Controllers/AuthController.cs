4d05bf16365c98aa5828e993cef99ff895a39ec1
Maksym <maksonchyks@gmail.com>
2025-07-10 13:14:22 +0300

    feat(api): added AuthController for login and register

---
+++ backend/MyFullStackNotes.Api/Controllers/AuthController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Internal;
+using Microsoft.EntityFrameworkCore.Query.Internal;
+using MyFullStackNotes.Api.DTO;
+using MyFullStackNotes.Application.Interfaces;
+
+namespace MyFullStackNotes.Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly IUserService _userService;
+        private readonly IJwtTokenGenerator _jwtTokenGenerator;
+        private readonly IPasswordHasher _passwordHasher;
+        public AuthController(IUserService userService, IJwtTokenGenerator jwtTokenGenerator, IPasswordHasher passwordHasher) 
+        {
+            _userService = userService;
+            _jwtTokenGenerator = jwtTokenGenerator;
+            _passwordHasher = passwordHasher;
+        }
+        [HttpPost("register")]
+        public async Task<ActionResult<AuthResponse>> Register(RegisterRequest registerRequest)
+        {
+            var user = await _userService.RegisterAsync(registerRequest.Email, registerRequest.Name, registerRequest.Password);
+            var token = _jwtTokenGenerator.GenerateToken(user);
+            return Ok(new AuthResponse
+            {
+                Token = token,
+                Email = user.Email,
+                Role = user.Role.ToString() //без ролі можливо краще повертати, чисто токен
+            });
+        }
+        [HttpPost("login")]
+        public async Task<ActionResult<AuthResponse>> Login(LoginRequest loginRequest) 
+        {
+            var user = await _userService.GetByEmailAsync(loginRequest.Email);
+
+            if(user == null || _passwordHasher.Verify(user.PasswordHash, loginRequest.Password))
+                return Unauthorized("Невірний email або пароль");
+
+            var token = _jwtTokenGenerator.GenerateToken(user);
+
+            return Ok(new AuthResponse
+            {
+                Token = token,
+                Email = user.Email,
+                Role = user.Role.ToString()
+            });
+        }
+    }
+}
