aba0c45f9821d1f646fa87a2dec2bc88fdc774ff
Maksonchyks <musinmaksim28@email.com>
2025-09-02 15:26:10 +0300

    Created new Dto for updating notes

---
+++ backend/MyFullStackNotes.Api/DTO/Note/UpdateNoteRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace MyFullStackNotes.Api.DTO.Note
+{
+    public class UpdateNoteRequest
+    {
+        public string? NewTitle { get; set; }
+        public required string Newdescription { get; set; }
+    }
+}
